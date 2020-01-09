646508e9bc614c7d7f7383388b36f89ec8c6a5f3
cc <chengchen.chn@outlook.com>
2020-01-09 13:26:20 +0800

    fix

--- data-structure-algorithm-cpp/main.cpp
+++ data-structure-algorithm-cpp/main.cpp
@@ -1,9 +1,4 @@
 #include <iostream>
-#include "Algorithm/Sort/SortTestHelper.h"
-#include "Algorithm/Sort/InsertionSort.h"
-#include "Algorithm/Sort/SelectionSort.h"
-#include "Algorithm/Sort/BubbleSort.h"
-
 #include "DataStructure/Array/Array.h"
 #include "DataStructure/List/LinkedList.h"
 #include "DataStructure/List/DLinkedList.h"
@@ -12,7 +7,6 @@
 #include "DataStructure/Stack/ArrayStack.h"
 #include "DataStructure/Stack/ListStack.h"
 
-using namespace SortTestHelper;
 using std::cout;
 using std::endl;
 
@@ -26,29 +20,4 @@ int main() {
 //    testSort("Bubble Sort", bubbleSort, arr_bubble_test, n);
 
 
-//    Array<int> array(5);
-//    cout << array << endl;
-//
-//    for (int i = 1; i < 11; i++) {
-//        array.add(i - 1, i - 1);
-//        cout << array << endl;
-//    }
-//
-//    for (int i = 0; i < 10; i++) {
-//        cout << array.remove(0) << endl;
-//        cout << array << endl;
-//    }
-
-    int arr[10];
-    for (int i = 0; i < 10; i ++)
-        arr[i] = i;
-
-    auto q = LoopQueue<int>();
-    for (int i = 0; i < 10; i++) {
-        q.enqueue(arr[i]);
-        cout << q << endl;
-        if (i % 3 == 1)
-            cout << q.dequeue() << endl;
-    }
-    return 0;
 }
