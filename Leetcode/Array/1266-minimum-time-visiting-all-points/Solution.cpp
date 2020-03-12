67776db3851d09ba571a1409908d9abbd7179f0d
cc <chengchen.chn@outlook.com>
2020-03-12 22:28:04 +0800

    WIP: update leetcode 1266

---
+++ Leetcode/Array/1266-minimum-time-visiting-all-points/Solution.cpp
@@ -0,0 +1,41 @@
+
+/**
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+#include <vector>
+#include <math.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int n = points.size(), res = 0;
+
+        for (int i = 1; i < n; i++)
+            res += max(abs(points[i][0] - points[i-1][0]), abs(points[i][1] - points[i-1][1]));
+
+        return res;
+    }
+};
