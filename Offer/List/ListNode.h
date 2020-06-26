e79e02781115fcf0c9338a147c2d2a56a9348b01
cc <chengchen.chn@outlook.com>
2020-06-26 20:44:40 +0800

    WIP: update offer 02.01

---
+++ Offer/List/ListNode.h
@@ -0,0 +1,21 @@
+
+#ifndef OFFER_LISTNODE_H
+#define OFFER_LISTNODE_H
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+#endif //OFFER_LISTNODE_H
