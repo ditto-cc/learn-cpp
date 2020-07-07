1f5544680da7800bf079a91c90e15e7c9b87169e
cc <chengchen.chn@outlook.com>
2020-07-07 10:07:36 +0800

    WIP: update leetcode 112

---
+++ Leetcode/Tree/0112-Path-Sum/Solution.cpp
@@ -0,0 +1,54 @@
+
+/*
+给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例: 
+给定如下二叉树，以及目标和 sum = 22，
+
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \      \
+        7    2      1
+返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#include "../TreeNode.h"
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+class Solution {
+private:
+    bool hasPath(TreeNode* root, int sum) {
+        if (!root)
+            return false;
+        if (sum == root->val && !root->left && !root->right)
+            return true;
+
+        sum -= root->val;
+        return hasPath(root->left, sum) || hasPath(root->right, sum);
+    }
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (!root)
+            return false;
+        return hasPath(root, sum);
+    }
+};
